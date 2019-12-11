f60e5fa2224826cd56e23bf1d591ffce4e3deb59
Bak-Pc <Bak-Pc@BAK-Labs>
2019-12-11 19:32:38 +0530

    Minor changes for imports

--- src/test/java/com/database/in28minutes/courseRepository/NativeQueriesTest.java
+++ src/test/java/com/database/in28minutes/courseRepository/NativeQueriesTest.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
-import javax.persistence.TypedQuery;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
