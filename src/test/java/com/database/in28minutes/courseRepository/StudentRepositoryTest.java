c9158c1515f794e69bd2e5913e0ab6508e53cd2b
Bak-Pc <Bak-Pc@BAK-Labs>
2019-12-12 17:17:45 +0530

    getStudentPassportDetails() added

---
+++ src/test/java/com/database/in28minutes/courseRepository/StudentRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.database.in28minutes.courseRepository;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.database.in28minutes.HibernateJpaIn100StepsApplication;
+import com.database.in28minutes.entity.Passport;
+import com.database.in28minutes.entity.Student;
+import com.database.in28minutes.repo.StudentRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = HibernateJpaIn100StepsApplication.class)
+
+public class StudentRepositoryTest {
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired(required = true)
+	StudentRepository studentRepository;
+	
+	@Autowired
+	EntityManager em;
+	
+	@Test
+	@Transactional
+	public void getStudentPassportDetails() {
+		Student student = em.find(Student.class, 101L);
+		log.info("Student Name=  "+ student.getName());
+		
+		 Passport number = student.getPassport();
+		log.info("passport number=  "+number.getNumber());
+	}
+
+}
