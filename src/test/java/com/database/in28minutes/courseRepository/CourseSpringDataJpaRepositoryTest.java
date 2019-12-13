7264291fe2e2331a32b985f7214bedcbe892ff85
Bak-Pc <Bak-Pc@BAK-Labs>
2019-12-13 19:19:37 +0530

    Test SpringDataJpaRepository with SORT() added

---
+++ src/test/java/com/database/in28minutes/courseRepository/CourseSpringDataJpaRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.database.in28minutes.courseRepository;
+
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.database.in28minutes.HibernateJpaIn100StepsApplication;
+import com.database.in28minutes.entity.Course;
+import com.database.in28minutes.repo.CourseSpringDataJpaRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = HibernateJpaIn100StepsApplication.class)
+
+public class CourseSpringDataJpaRepositoryTest {
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired(required = true)
+	CourseSpringDataJpaRepository courseDataJpaRepository;
+
+	@Test
+	public void findByID_If_Present() {
+		Optional<Course> findById = courseDataJpaRepository.findById(14L);
+		log.info("Course Name=  " + findById.isPresent());
+	}
+
+	@Test
+	public void findByID_If_Not_Present() {
+		Optional<Course> findById = courseDataJpaRepository.findById(18L);
+		log.info("Course Name=  " + findById.isPresent());
+	}
+
+	@Test
+	public void SortAllCourses() {
+		
+		log.info("Couse Count=  "+courseDataJpaRepository.count());
+	
+		log.info("All Courses sorted DESC order =  "+courseDataJpaRepository.findAll(Sort.by(Direction.DESC,"name")));
+	
+	}
+}
